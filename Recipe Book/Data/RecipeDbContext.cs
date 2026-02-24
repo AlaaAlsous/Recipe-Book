a0e5ce52c250c09136026d8412dd56202484a7f6
Alaa Alsous <lawyeralaa@gmail.com>
2026-02-24 18:17:04 +0100

    Set up relationships and keys in RecipeDbContext

--- Recipe Book/Data/RecipeDbContext.cs
+++ Recipe Book/Data/RecipeDbContext.cs
@@ -16,5 +16,33 @@ namespace Recipe_Book.Data
 			optionsBuilder.UseSqlServer("Server=localhost,1433;Database=net25_db;User Id=net25;Password=Secret-NET.25-Password!;TrustServerCertificate=True;Encrypt=True;");
 		}
 
+		protected override void OnModelCreating(ModelBuilder modelBuilder)
+		{
+			modelBuilder.Entity<RecipeIngredient>()
+				.HasKey(ri => new { ri.RecipeId, ri.IngredientId });
+
+			modelBuilder.Entity<RecipeIngredient>()
+				.HasOne(ri => ri.Recipe)
+				.WithMany(r => r.RecipeIngredients)
+				.HasForeignKey(ri => ri.RecipeId);
+
+			modelBuilder.Entity<RecipeIngredient>()
+				.HasOne(ri => ri.Ingredient)
+				.WithMany(i => i.RecipeIngredients)
+				.HasForeignKey(ri => ri.IngredientId);
+
+			modelBuilder.Entity<RecipeCategory>()
+				.HasKey(rc => new { rc.RecipeId, rc.CategoryId });
+
+			modelBuilder.Entity<RecipeCategory>()
+				.HasOne(rc => rc.Recipe)
+				.WithMany(r => r.RecipeCategories)
+				.HasForeignKey(rc => rc.RecipeId);
+
+			modelBuilder.Entity<RecipeCategory>()
+				.HasOne(rc => rc.Category)
+				.WithMany(c => c.RecipeCategories)
+				.HasForeignKey(rc => rc.CategoryId);
+		}
 	}
 }
