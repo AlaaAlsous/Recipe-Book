5c021a01914b416bb3de4e2c2a39c9eda20c6933
Alaa Alsous <lawyeralaa@gmail.com>
2026-05-20 22:18:26 +0200

    Refactor DbContext to support DI-based SQL Server and SQLite fallback configuration

--- Data/RecipeDbContext.cs
+++ Data/RecipeDbContext.cs
@@ -3,50 +3,48 @@ using Recipe_Book.Models;
 
 namespace Recipe_Book.Data
 {
-	public class RecipeDbContext : DbContext
-	{
-		public DbSet<Recipe> Recipes { get; set; }
-		public DbSet<Ingredient> Ingredients { get; set; }
-		public DbSet<Category> Categories { get; set; }
-		public DbSet<RecipeIngredient> RecipeIngredients { get; set; }
-		public DbSet<RecipeCategory> RecipeCategories { get; set; }
+    public class RecipeDbContext : DbContext
+    {
+        public RecipeDbContext(DbContextOptions<RecipeDbContext> options)
+            : base(options){}
 
-		protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-		{
-			optionsBuilder.UseSqlServer("Server=localhost,1433;Database=recipe_book_db;User Id=recipebook;Password=Secret-Recipe-Book-Password!;TrustServerCertificate=True;Encrypt=True;");
-		}
+        public DbSet<Recipe> Recipes { get; set; }
+        public DbSet<Ingredient> Ingredients { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<RecipeIngredient> RecipeIngredients { get; set; }
+        public DbSet<RecipeCategory> RecipeCategories { get; set; }
 
-		protected override void OnModelCreating(ModelBuilder modelBuilder)
-		{
-			modelBuilder.Entity<RecipeIngredient>()
-				.HasKey(ri => new { ri.RecipeId, ri.IngredientId });
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<RecipeIngredient>()
+                .HasKey(ri => new { ri.RecipeId, ri.IngredientId });
 
-			modelBuilder.Entity<RecipeIngredient>()
-				.HasOne(ri => ri.Recipe)
-				.WithMany(r => r.RecipeIngredients)
-				.HasForeignKey(ri => ri.RecipeId)
-				.OnDelete(DeleteBehavior.Cascade);
+            modelBuilder.Entity<RecipeIngredient>()
+                .HasOne(ri => ri.Recipe)
+                .WithMany(r => r.RecipeIngredients)
+                .HasForeignKey(ri => ri.RecipeId)
+                .OnDelete(DeleteBehavior.Cascade);
 
-			modelBuilder.Entity<RecipeIngredient>()
-				.HasOne(ri => ri.Ingredient)
-				.WithMany(i => i.RecipeIngredients)
-				.HasForeignKey(ri => ri.IngredientId)
-				.OnDelete(DeleteBehavior.Restrict);
+            modelBuilder.Entity<RecipeIngredient>()
+                .HasOne(ri => ri.Ingredient)
+                .WithMany(i => i.RecipeIngredients)
+                .HasForeignKey(ri => ri.IngredientId)
+                .OnDelete(DeleteBehavior.Restrict);
 
-			modelBuilder.Entity<RecipeCategory>()
-				.HasKey(rc => new { rc.RecipeId, rc.CategoryId });
+            modelBuilder.Entity<RecipeCategory>()
+                .HasKey(rc => new { rc.RecipeId, rc.CategoryId });
 
-			modelBuilder.Entity<RecipeCategory>()
-				.HasOne(rc => rc.Recipe)
-				.WithMany(r => r.RecipeCategories)
-				.HasForeignKey(rc => rc.RecipeId)
-				.OnDelete(DeleteBehavior.Cascade);
+            modelBuilder.Entity<RecipeCategory>()
+                .HasOne(rc => rc.Recipe)
+                .WithMany(r => r.RecipeCategories)
+                .HasForeignKey(rc => rc.RecipeId)
+                .OnDelete(DeleteBehavior.Cascade);
 
-			modelBuilder.Entity<RecipeCategory>()
-				.HasOne(rc => rc.Category)
-				.WithMany(c => c.RecipeCategories)
-				.HasForeignKey(rc => rc.CategoryId)
-				.OnDelete(DeleteBehavior.Restrict);
-		}
-	}
+            modelBuilder.Entity<RecipeCategory>()
+                .HasOne(rc => rc.Category)
+                .WithMany(c => c.RecipeCategories)
+                .HasForeignKey(rc => rc.CategoryId)
+                .OnDelete(DeleteBehavior.Restrict);
+        }
+    }
 }
--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Data.SqlClient;
 using Recipe_Book.Data;
 using Recipe_Book.Services;
 
@@ -12,7 +14,23 @@ namespace Recipe_Book
             ApplicationConfiguration.Initialize();
 
             var services = new ServiceCollection();
-            services.AddDbContext<RecipeDbContext>();
+
+            string connectionString =
+                "Server=localhost,1433;Database=recipe_book_db;User Id=recipebook;Password=Secret-Recipe-Book-Password!;TrustServerCertificate=True;Encrypt=True;";
+
+            bool dockerAvailable = CanConnectToDockerDb(connectionString);
+
+            if (dockerAvailable)
+            {
+                services.AddDbContext<RecipeDbContext>(options =>
+                    options.UseSqlServer(connectionString));
+            }
+            else
+            {
+                services.AddDbContext<RecipeDbContext>(options =>
+                    options.UseSqlite("Data Source=Recipe-Book.sqlite"));
+            }
+
             services.AddScoped<RecipeService>();
             services.AddTransient<MainForm>();
 
@@ -33,6 +51,20 @@ namespace Recipe_Book
             }
 
             Application.Run(serviceProvider.GetRequiredService<MainForm>());
+        }
+
+        private static bool CanConnectToDockerDb(string connectionString)
+        {
+            try
+            {
+                using var conn = new SqlConnection(connectionString);
+                conn.Open();
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
         }
     }
 }
